809c4251619b9e1f8396bebae399fe3c757a0a0e
stanley <stanleyflofer@gmail.com>
2017-12-21 17:36:08 +0530

    fibo using tail recurssion, fibo using one function, isbn more validations, rail fence cipher

--- app/isbn.hs
+++ app/isbn.hs
@@ -13,7 +13,7 @@ checkX :: String -> String
 checkX xs =     if( (last(xs) )=='x')
                     then init(xs)               
                 else if ( (last(xs))>='a' || (last(xs))>='A')
-                    then init(xs) ++ ['0']
+                    then "1234567890"
                 else xs
 
 
@@ -32,10 +32,10 @@ validateIsbn :: String -> Bool
 validateIsbn xs = (checkIsbn (changeToInt(removeHypen xs)) )
 
 main = do
-  print (validateIsbn "3-598-21508-8")            --true
-  print (validateIsbn "3662392003")              --true
-  print (validateIsbn"3-598-21507-xx")          --false
-  print (validateIsbn "3-598-21508-9")            --false
+  print (validateIsbn "3-598-21508-8")             --true
+  print (validateIsbn "3662392003")                --true
+  print (validateIsbn"3-598-21507-xx")             --false
+  print (validateIsbn "3-598-21508-9")             --false
   print (validateIsbn "3-598-21508-111")           --false
   print (validateIsbn "3598215088")                --true
   print (validateIsbn "3598a15088")                --false
@@ -44,6 +44,6 @@ main = do
   print (validateIsbn "3-423423-42822-22")         --false 
   print (validateIsbn "3-598-21507-A")             --false
   print (validateIsbn "3-598-21507-S")             --false
-  print (validateIsbn "123-45")                      --false
-
-  
+  print (validateIsbn "123-45")                    --false
+  print (validateIsbn "123-45-0000-A")             --false
+  print (validateIsbn "123-45-0000-x")             --false
