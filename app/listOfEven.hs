74d67321c3439c03f3c0ee800c3c506d0e898490
stanley <stanleyflofer@gmail.com>
2017-12-23 18:03:08 +0530

    list of even number, sum of n multiples of 3 and 5

---
+++ app/listOfEven.hs
@@ -0,0 +1,6 @@
+evenList :: Int -> [Int] -> [Int]
+evenList n lst = if (n==0)
+                    then lst
+                 else if(n `mod` 2 ==0) 
+                    then evenList(n-1) (n:lst)
+                 else evenList(n-1) lst
---
+++ app/practice3.hs
@@ -0,0 +1,22 @@
+import Data.Char
+import Data.List
+
+
+main = do
+    print  ( map (\x -> toUpper x) "abc" )
+    print  ( map (\i -> i * 2) [1, 2, 3] )
+    print  ( map (\c -> digitToInt c) "3345" )
+    print  ( filter (\x -> (mod x 2) == 0) [1, 2, 3, 4, 5, 6] )
+    print  ( filter (\x -> isAlpha x) "Hello world!" )
+    print  ( filter (\x -> x /= "") ["hello", "", "world"] )
+    print  ( foldl' (\result x -> if (x `elem` result) then result else (x:result)) [] [1, 2, 3, 3, 2, 3, 1] )
+    print  ( foldl' (\result x -> result ++ [x * 2]) [] [1, 2, 3] )
+    print  ( map (\x -> x * 2) [1, 2, 3] )
+    print  ( divMod 10 2 )
+
+
+
+
+
+
+
