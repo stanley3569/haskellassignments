ea842961018a2a7704da3a135f72e4f7ad5a2996
stanley <stanleyflofer@gmail.com>
2017-12-26 12:44:41 +0530

    modules

--- src/chapter1/leapyear.hs
+++
@@ -1,7 +0,0 @@
-leapyear :: Int -> Bool
-leapyear x = ((x `mod` 4 ==0) && ((x `mod` 100 /=0)||(x `mod` 400 ==0)))
-
-
-main = do
-    print $ leapyear 2016
-    print $ leapyear 1993
--- src/chapter1/listOfEven1.hs
+++
@@ -1,7 +0,0 @@
-evenList :: Int -> [Int] -> [Int]
-evenList n lst = if (n==0)
-                    then lst
-                 else if(n `mod` 2 ==0) 
-                    then evenList(n-1) ([n]++lst)
-                 else evenList(n-1) lst
-                
