e2a7b27684a029704fc902c5c474db77070885eb
stanley <stanleyflofer@gmail.com>
2017-12-26 11:49:53 +0530

    chapter1

--- app/practice3.hs
+++ app/practice3.hs
@@ -2,17 +2,23 @@ import Data.Char
 import Data.List
  
 
+
+calculateEndTime :: Int -> Int -> Int -> (Int, Int)
+calculateEndTime hr mn durationInMins = let (addHour, finalMins) = divMod (mn + durationInMins) 60
+                                        in (hr + addHour, finalMins)
+
 main = do
     print  ( map (\x -> toUpper x) "abc" )
-    print  ( map (\i -> i * 2) [1, 2, 3] )
-    print  ( map (\c -> digitToInt c) "3345" )
-    print  ( filter (\x -> (mod x 2) == 0) [1, 2, 3, 4, 5, 6] )
-    print  ( filter (\x -> isAlpha x) "Hello world!" )
-    print  ( filter (\x -> x /= "") ["hello", "", "world"] )
-    print  ( foldl' (\result x -> if (x `elem` result) then result else (x:result)) [] [1, 2, 3, 3, 2, 3, 1] )
-    print  ( foldl' (\result x -> result ++ [x * 2]) [] [1, 2, 3] )
-    print  ( map (\x -> x * 2) [1, 2, 3] )
-    print  ( divMod 10 2 )
+    --print  ( map (\i -> i * 2+i) [1, 2, 3] )
+   --print  ( map (\c -> digitToInt c) "3345" )
+   -- print(filter (\x -> length x>4) ["aaaa","bbbbbbbbb","c"])
+   --print  ( filter (\x -> (mod x 2) == 0) [1, 2, 3, 4, 5, 6] )
+  --  print  ( filter (\x -> isAlpha x) "Hello world!" )
+   -- print  ( filter (\x -> x /= "") ["hello", "", "world"] )
+   -- print  ( foldl' (\result x -> if (x `elem` result) then result else (x:result)) [] [1, 2, 3, 3, 2, 3, 1] )
+   -- print  ( foldl' (\result x -> result ++ [x * 2]) [] [1, 2, 3] )
+   -- print  ( map (\x -> x * 2) [1, 2, 3] )
+   -- print  ( divMod 10 2 )
 
 
 
---
+++ src/chapter1/sumSquareDiff1.hs
@@ -0,0 +1,13 @@
+sumOfSquare :: Int -> Int -> Int -> Int
+sumOfSquare n x sum = if(n<=x)
+                        then sumOfSquare(n+1)  x (sum+(n*n))
+                    else sum
+
+squareOfSum :: Int -> Int -> Int -> Int
+squareOfSum n x sum = if(n<=x)
+                        then squareOfSum(n+1)  x (sum+n)
+                    else sum*sum
+
+
+difference :: Int -> Int
+difference x = squareOfSum 0 x 0 - sumOfSquare 0 x 0
