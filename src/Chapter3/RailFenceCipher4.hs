8baffa2520c075c12d67ff200da054bdf2276fdf
stanley <stanleyflofer@gmail.com>
2018-01-18 17:54:35 +0530

    chap3

--- src/Chapter3/RailFenceCipher4.hs
+++ src/Chapter3/RailFenceCipher4.hs
@@ -1,75 +1,32 @@
 module Chapter3.RailFenceCipher4 where
 
 
-
 withoutSpaces :: String -> String
 withoutSpaces xs = (filter (/=' ') xs)
    
-data Rail = MkRail
-    {
-        inputString :: String,
-        rail1 :: String,
-        rail2 :: String,
-        rail3 :: String,
-        currentRail :: Int,
-        movementRail :: Int
-    } deriving (Eq,Show,Ord)
+
+
+data StartRails1 = MkStartRails1 String deriving (Eq, Show,Ord)
+data StartRails2 = MkStartRails2 String deriving (Eq, Show,Ord)
+data StartRails3 = MkStartRails3 String deriving (Eq, Show,Ord)
 
 
 
-encodeMessage :: Rail -> String
-encodeMessage MkRail{inputString=ips, rail1=r1, rail2=r2, rail3=r3, currentRail=cr,movementRail=mr}=
-    {-}
-    let MkRail = if(inputString /= "") 
-                then  
-                    if(currentRail == 1)
-                        then encodeMessage (tail inputString) (rail1++[(head inputString)]) rail2 rail3 (currentRail-movementDirection) (movementDirection)
-                    else if(currentRail ==2)
-                        then encodeMessage (tail inputString) rail1 (rail2++[(head inputString)]) rail3 (currentRail-(movementDirection)) (-movementDirection)
-                    else encodeMessage (tail inputString) rail1 rail2 (rail3++[(head inputString)]) (currentRail-1) (movementDirection)
-             else rail1++rail2++rail3
-    -}
-{-}
-encodeMessage :: String -> String -> String -> String -> Int -> Int -> String 
-encodeMessage inputString rail1 rail2 rail3 currentRail movementDirection = if(inputString /= "") 
+
+encodeMessage :: String -> StartRails1 -> StartRails2 -> StartRails3 -> Int -> Int -> (StartRails1,StartRails2,StartRails3) 
+encodeMessage inputString (MkStartRails1 rail1) (MkStartRails2 rail2) (MkStartRails3 rail3) currentRail movementDirection = 
+                                                                            if(inputString /= "") 
                                                                                 then  
                                                                                     if(currentRail == 1)
-                                                                                        then encodeMessage (tail inputString) (rail1++[(head inputString)]) rail2 rail3 (currentRail-movementDirection) (movementDirection)
+                                                                                        then encodeMessage (tail inputString) (MkStartRails1 (rail1++[(head inputString)]))  (MkStartRails2 rail2) (MkStartRails3 rail3) (currentRail-movementDirection) (movementDirection)
                                                                                     else if(currentRail ==2)
-                                                                                        then encodeMessage (tail inputString) rail1 (rail2++[(head inputString)]) rail3 (currentRail-(movementDirection)) (-movementDirection)
-                                                                                    else encodeMessage (tail inputString) rail1 rail2 (rail3++[(head inputString)]) (currentRail-1) (movementDirection)
-                                                                            else rail1++rail2++rail3
- -}
-                                                                            
-
-
-
-
-
-
-
-
-          {-}                                                                    
+                                                                                        then encodeMessage (tail inputString) (MkStartRails1 rail1) (MkStartRails2 (rail2++[(head inputString)])) (MkStartRails3 rail3) (currentRail-(movementDirection)) (-movementDirection)
+                                                                                    else encodeMessage (tail inputString) (MkStartRails1 rail1) (MkStartRails2 rail2) (MkStartRails3 (rail3++[(head inputString)])) (currentRail-1) (movementDirection)
+                                                                            else ( (MkStartRails1 rail1),(MkStartRails2 rail2),(MkStartRails3 rail3) )
+                                                                           
+                                                                              
 main = do
-        print ( encodeMessage (withoutSpaces "WE ARE DISCOVERED FLEE AT ONCE") [] [] [] 1 (-1))           --WECRLTEERDSOEEFEAOCAIVDEN
-        print ( encodeMessage (withoutSpaces "helloworld") [] [] [] 1 (-1))                               --holelwrdlo
-        print ( encodeMessage (withoutSpaces "hello") [] [] [] 1 (-1))                                    --hoell
-        print ( encodeMessage (withoutSpaces "1234567890") [] [] [] 1 (-1))                               --1592468037
-
--}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        print ( encodeMessage (withoutSpaces "WE ARE DISCOVERED FLEE AT ONCE") (MkStartRails1 []) (MkStartRails2 []) (MkStartRails3 []) 1 (-1))           --WECRLTEERDSOEEFEAOCAIVDEN
+        print ( encodeMessage (withoutSpaces "helloworld") (MkStartRails1 []) (MkStartRails2 []) (MkStartRails3 []) 1 (-1))                               --holelwrdlo
+        print ( encodeMessage (withoutSpaces "hello") (MkStartRails1 []) (MkStartRails2 []) (MkStartRails3 []) 1 (-1))                                    --hoell
+        print ( encodeMessage (withoutSpaces "1234567890") (MkStartRails1 []) (MkStartRails2 []) (MkStartRails3 []) 1 (-1))                               --1592468037
